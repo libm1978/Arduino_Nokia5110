ae2bcca23f0b323838cd9bf5126846e87ac665f1
libm1978 <libm1978@hotmail.com>
2017-07-04 15:15:19 +0800

    添加了SPI接口类，为SPI功能提供抽象

---
+++ Nokia5110/SPITransfer.h
@@ -0,0 +1,20 @@
+/*
+ * SPITransfer.h
+ *
+ *  Created on: 2017年7月4日
+ *      Author: li
+ */
+
+#ifndef NOKIA5110_SPITRANSFER_H_
+#define NOKIA5110_SPITRANSFER_H_
+
+//功能：为SPI接口提供抽象
+class SPITransfer
+{
+
+};
+
+
+
+
+#endif /* NOKIA5110_SPITRANSFER_H_ */
